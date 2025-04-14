54c8f561497ad3b661ef1fb8bc20d5c490ec4374
Shuo Gao <gshuo.666@gmail.com>
2025-04-14 13:41:23 +0800

    1.2

--- src/main/java/com/example/dhf_springboot/model/Caculator.java
+++ src/main/java/com/example/dhf_springboot/model/Caculator.java
@@ -1,12 +1,44 @@
 package com.example.dhf_springboot.model;
 
+import com.example.dhf_springboot.vo.CalculatorForm;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * FileName: caculator.java
- * 类的详细说明
+ * 负责计算器的逻辑
  *
  * @author GaoShuo
  * @version 1.0.0
  * @Date 2025/4/14
  */
 public class Caculator {
+
+//    CalculatorForm calculatorForm;
+
+    public double calculate(CalculatorForm calculatorForm) {
+//        this.calculatorForm = calculatorForm;
+        double result;
+        double num1 = calculatorForm.getNum1();
+        double num2 = calculatorForm.getNum2();
+
+        switch (calculatorForm.getOperator()) {
+            case "+":
+                result = num1 + num2; break;
+            case "-":
+                result = num1 - num2; break;
+            case "*":
+                result = num1 * num2; break;
+            case "/":
+                if (num2 == 0) {
+                    throw new ArithmeticException("不能除以0");
+                }
+                result = num1 / num2; break;
+            default:
+                throw new IllegalArgumentException("不支持的运算符");
+        }
+
+        return result;
+    }
+
+
 }
---
+++ src/main/java/com/example/dhf_springboot/vo/CalculatorForm.java
@@ -0,0 +1,52 @@
+package com.example.dhf_springboot.vo;
+
+import lombok.Data;
+
+/**
+ * FileName: Calculator.java
+ * 前端传入的计算器表单
+ *
+ * @author GaoShuo
+ * @version 1.0.0
+ * @Date 2025/4/14
+ */
+@Data
+public class CalculatorForm {
+    double num1;
+    double num2;
+    String operator;
+
+    double result;
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+}
